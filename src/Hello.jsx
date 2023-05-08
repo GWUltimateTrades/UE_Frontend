a4f8c18a3b6371c6d7a1f9433848dd3c8dfa83ba
plebbbb <ili310465@gmail.com>
2023-05-08 15:21:07 -0400

    initial setup

---
+++ src/Hello.jsx
@@ -0,0 +1,12 @@
+//import "Tachyons";
+
+const Hello = () => (
+    <div>
+        <h1>testing</h1>
+        <div className="flex">
+            <div class="outline w-25 pa3">amogus</div>
+            <div class="outline w-25 pa3">amogus</div>
+        </div>
+    </div>
+)
+export default Hello;
