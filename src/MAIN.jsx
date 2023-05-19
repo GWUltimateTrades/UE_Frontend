81a1bb943c44b6a715a84f6e773c0ca0fb7d78ba
plebbbb <ili310465@gmail.com>
2023-05-19 19:39:00 -0400

    fix again

--- src/MAIN.jsx
+++ src/MAIN.jsx
@@ -1,11 +1,11 @@
 import Top_bar from "./Top_bar";
+import Logo_title from "./Logo_title";
 import About_us from "./About_us";
 import Content_Test from "./Content_Test";
 const MAIN = () => (
-    <div>
-        <Top_bar></Top_bar>
-        <About_us></About_us>
-        <Content_Test></Content_Test>
+    <div className = "flex flex-column w-100 h-100 items-center pv7">
+        <Logo_title font = 'helvetica' color = 'black-80'></Logo_title>
+        <div className =  'helvetica'>Website Coming Soon</div>
     </div>
 )
 
