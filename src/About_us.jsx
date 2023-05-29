9dc61ec6b06b269973138197b50e691f0d70adf2
plebbbb <ili310465@gmail.com>
2023-05-29 16:39:51 -0400

    Test

--- src/About_us.jsx
+++ src/About_us.jsx
@@ -6,7 +6,7 @@ const About_us = () => {
     const body = "Here at GW Ultimate Trades, we offer electrical contracting for residential, industrial and commercial applications. Our highly experienced team provides the utmost in service to ensure an excellent experience."
     
     return (
-        <STD_text title={title} body = {body} font = "georgia"></STD_text>
+        <STD_text body = {body} font = "georgia"></STD_text>
     )
 }
 
--- src/Top_bar_submenu.jsx
+++ src/Top_bar_submenu.jsx
@@ -1,10 +1,10 @@
 const Top_bar_submenu = (props) => (
     <div>
         <div className = {`${props.color} flex justify-center items-start pa1`}>
-            <div className = {`pa2 ${props.font} fw6 w-25`}>TOP BAR SUBMENU</div>
-            <div className = {`pa2 ${props.font} fw6 w-25`}>TOP BAR SUBMENU</div>
-            <div className = {`pa2 ${props.font} fw6 w-25`}>TOP BAR SUBMENU</div>
-            <div className = {`pa2 ${props.font} fw6 w-25`}>TOP BAR SUBMENU</div>
+            <div className = {`pa2 ${props.font} fw6 w-25`}>CONTRACTING</div>
+            <div className = {`pa2 ${props.font} fw6 w-25`}>SERVICE</div>
+            <div className = {`pa2 ${props.font} fw6 w-25`}>CAREER</div>
+            <div className = {`pa2 ${props.font} fw6 w-25`}>TRAINING CENTER</div>
         </div>
     </div>
 )
